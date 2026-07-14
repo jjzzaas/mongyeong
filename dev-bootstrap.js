4606aa12b52011547dab75b411760babb2295050
jjzzaas <jojicheon1@gmail.com>
2026-07-14 20:32:09 +0900

    임시 메인 로비 바로가기 부트스트랩 추가

---
+++ dev-bootstrap.js
@@ -0,0 +1,8 @@
+(()=>{
+  if(sessionStorage.getItem('mongyeong.devLobby')!=='1')return;
+  window.CHAPTER_1=[{
+    type:'mainLobby',
+    speaker:'하루',
+    text:'우선 길드부터 가보자.\n네가 이곳 사람이라면 뭔가 기록이 남아 있을지도 몰라.\n나도 임무 완료 보고를 해야 하니까, 어차피 가야 하거든.'
+  }];
+})();
