d6e65533462e3636a36d221d03eda6e7c50acbdf
jjzzaas <jojicheon1@gmail.com>
2026-07-28 11:58:48 +0900

    Chapter 11-14 로더 연결

--- vn-chapter10-loader.js
+++ vn-chapter10-loader.js
@@ -1,4 +1,4 @@
-const entryUrl = new URL('./vn-3.2.js?v=vn-3.2-chapter10-source', window.location.href);
+const entryUrl = new URL('./vn-3.2.js?v=vn-3.2-chapter14-source', window.location.href);
 const response = await fetch(entryUrl, { cache: 'no-store' });
 if (!response.ok) throw new Error(`비주얼 노벨 엔트리 로드 실패: ${response.status}`);
 
@@ -7,11 +7,16 @@ const baseUrl = new URL('./', window.location.href).href;
 source = source.replaceAll("from './", `from '${baseUrl}`);
 source = source.replace(
   "import { chapter009 } from '" + baseUrl + "src/chapters/001-010/chapter-009.js';",
-  "import { chapter009 } from '" + baseUrl + "src/chapters/001-010/chapter-009.js';\nimport { chapter010 } from '" + baseUrl + "src/chapters/001-010/chapter-010.js';",
+  "import { chapter009 } from '" + baseUrl + "src/chapters/001-010/chapter-009.js';\n" +
+  "import { chapter010 } from '" + baseUrl + "src/chapters/001-010/chapter-010.js';\n" +
+  "import { chapter011 } from '" + baseUrl + "src/chapters/011-020/chapter-011.js';\n" +
+  "import { chapter012 } from '" + baseUrl + "src/chapters/011-020/chapter-012.js';\n" +
+  "import { chapter013 } from '" + baseUrl + "src/chapters/011-020/chapter-013.js';\n" +
+  "import { chapter014 } from '" + baseUrl + "src/chapters/011-020/chapter-014.js';",
 );
 source = source.replace(
   'const chapters = [chapter001, chapter002, chapter003, chapter004, chapter005, chapter006, chapter007, chapter008, chapter009];',
-  'const chapters = [chapter001, chapter002, chapter003, chapter004, chapter005, chapter006, chapter007, chapter008, chapter009, chapter010];',
+  'const chapters = [chapter001, chapter002, chapter003, chapter004, chapter005, chapter006, chapter007, chapter008, chapter009, chapter010, chapter011, chapter012, chapter013, chapter014];',
 );
 
 const moduleUrl = URL.createObjectURL(new Blob([source], { type: 'text/javascript' }));
